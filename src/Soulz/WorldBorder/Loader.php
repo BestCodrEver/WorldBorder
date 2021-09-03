9f04f10d76e67f5198f5a5dbab376126aad0d6ba
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-09-03 11:49:14 -0700

    Implements Fling

--- src/Soulz/WorldBorder/Loader.php
+++ src/Soulz/WorldBorder/Loader.php
@@ -4,13 +4,17 @@ namespace Soulz\WorldBorder;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
-class WorldBorder extends PluginBase implements Listener {
+class Loader extends PluginBase implements Listener {
+
+    /** @var walkCooldown */
+    private $walkCooldown = [];
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -34,11 +38,29 @@ class WorldBorder extends PluginBase implements Listener {
             $v2 = new Vector3($player->x, 0, $player->z);
 
             if($v2->distance($v1) > $dat[$level->getName()]){
-                $event->setCancelled();
-                $player->sendMessage(Utils::RED_PREFIX . $this->getConfig()->get("border.border-message") . TextFormat::RESET);
+                if(!isset($this->walkCooldown[$player->getName()]) or $this->walkCooldown[$player->getName()] > 3){
+			              $player->sendMessage(Utils::RED_PREFIX . TextFormat::GRAY . $this->getConfig()->getNested("border.border-message"));
+			              $player->setMotion($event->getFrom()->subtract($player->getLocation())->normalize()->multiply(2));					
+			              $this->walkCooldown[$player->getName()] = 0;
+		            } else {
+			              $this->walkCooldown[$player->getName()] = $this->walkCooldown[$player->getName()] + 1;
+		          
+		            }
             }
 
         }
 
     }
+
+    
+    /**
+     * @param PlayerQuitEvent $event
+     */
+    public function onQuit(PlayerQuitEvent $event) {
+		    $playerName = $event->getPlayer()->getName();
+
+		    if(isset($this->walkCooldown[$playerName])){
+			      unset($this->walkCooldown[$playerName]);
+		    }
+	  }
 }
